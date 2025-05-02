26d8369b087b317ecce6c4e8633faa2b1749aa16
Abhay Pawar <abhaypawar0817@gmail.com>
2025-05-02 17:59:24 +0530

    fixed error in language api

--- models/language.model.js
+++ models/language.model.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  return sequelize.define('language', {
+  return sequelize.define('Language', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
--- plugins/sequelize.js
+++ plugins/sequelize.js
@@ -1,6 +1,7 @@
 const fp = require('fastify-plugin');
 const sequelize = require('../config/db');
 const ProductModel = require('../models/product.model');
+const LanguageModel = require('../models/language.model');
 
 async function dbPlugin(fastify, options) {
   try {
@@ -8,11 +9,12 @@ async function dbPlugin(fastify, options) {
     console.log('Database connected.');
 
     const Product = ProductModel(sequelize);
+    const Language = LanguageModel(sequelize);
 
     await sequelize.sync();
 
     fastify.decorate('db', sequelize);
-    fastify.decorate('models', { Product });
+    fastify.decorate('models', { Product, Language });
   } catch (err) {
     console.error('DB Connection Failed:', err);
     process.exit(1);
