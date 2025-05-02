e0f45bd6cbe88d91ee9fd796ba0f97d7341aad78
Abhay Pawar <abhaypawar0817@gmail.com>
2025-05-03 00:11:16 +0530

    changed language controller

--- controllers/language.controller.js
+++ controllers/language.controller.js
@@ -1,30 +1,39 @@
 exports.getSelectedLanguage = async (req, res) => {
-    try {
-      const id = req.params.id || 1;
-      const lang = await req.server.models.Language.findByPk(id);
-  
-      if (!lang) {
-        return res.status(404).send({ message: 'Language details not found' });
-      }
-  
-      return res.status(200).send(lang);
-    } catch (error) {
-      return res.status(500).send({ message: 'Server error', error: error.message });
+  try {
+    const id = req.params.id;
+    const lang = await req.server.models.Language.findByPk(id);
+
+    if (!lang) {
+      return res.status(404).send({ message: 'Language details not found' });
     }
-  };
-  
-  exports.postLanguageDetails = async (req, res) => {
-    try {
-      const { home, our_customers, about_us, contact_us, button, paragraph } = req.body;
-  
-      if (!home || !our_customers || !about_us || !contact_us || !button || !paragraph) {
-        return res.status(400).send({ message: 'All fields are required' });
-      }
-  
-      const newLang = await req.server.models.Language.create(req.body);
-  
-      return res.status(201).send(newLang);
-    } catch (error) {
-      return res.status(500).send({ message: 'Server error', error: error.message });
+
+    return res.status(200).send(lang);
+  } catch (error) {
+    return res.status(500).send({ message: 'Server error', error: error.message });
+  }
+};
+
+exports.postLanguageDetails = async (req, res) => {
+  try {
+    const { home, our_customers, about_us, contact_us, button, paragraph, order, terms } = req.body;
+
+    if (!home || !our_customers || !about_us || !contact_us || !button || !paragraph || !order || !terms) {
+      return res.status(400).send({ message: 'All fields are required' });
     }
-  };
+
+    const newLang = await req.server.models.Language.create({
+      home,
+      order,
+      our_customers,
+      about_us,
+      contact_us,
+      terms,
+      button,
+      paragraph,
+    });
+
+    return res.status(201).send(newLang);
+  } catch (error) {
+    return res.status(500).send({ message: 'Server error', error: error.message });
+  }
+};
